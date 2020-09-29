88d0a6e8d16416b5c9f4730db7440eccb9c63c56
Jack J. Woehr <jwoehr@softwoehr.com>
2020-09-29 09:39:16 -0600

    moving more stuff over and playing with setup.py

--- setup.py
+++ setup.py
@@ -10,11 +10,17 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-"The Qiskit OpenQASM2 setup file."
+"The Qiskit Terra setup file."
 
 import os
-from setuptools import setup, find_packages
-
+import sys
+from setuptools import setup, find_packages, Extension
+try:
+    from Cython.Build import cythonize
+except ImportError:
+    import subprocess
+    subprocess.call([sys.executable, '-m', 'pip', 'install', 'Cython>=0.27.1'])
+    from Cython.Build import cythonize
 
 REQUIREMENTS = [
     "contextvars>=2.4;python_version<'3.7'",
@@ -32,6 +38,37 @@ REQUIREMENTS = [
     "python-dateutil>=2.8.0",
 ]
 
+# Add Cython extensions here
+CYTHON_EXTS = ['utils', 'swap_trial']
+CYTHON_MODULE = 'qiskit.transpiler.passes.routing.cython.stochastic_swap'
+CYTHON_SOURCE_DIR = 'qiskit/transpiler/passes/routing/cython/stochastic_swap'
+
+INCLUDE_DIRS = []
+# Extra link args
+LINK_FLAGS = []
+# If on Win and not in MSYS2 (i.e. Visual studio compile)
+if (sys.platform == 'win32' and os.environ.get('MSYSTEM') is None):
+    COMPILER_FLAGS = ['/O2']
+# Everything else
+else:
+    COMPILER_FLAGS = ['-O2', '-funroll-loops', '-std=c++11']
+    if sys.platform == 'darwin':
+        # These are needed for compiling on OSX 10.14+
+        COMPILER_FLAGS.append('-mmacosx-version-min=10.9')
+        LINK_FLAGS.append('-mmacosx-version-min=10.9')
+
+
+EXT_MODULES = []
+# # Add Cython Extensions
+# for ext in CYTHON_EXTS:
+#     mod = Extension(CYTHON_MODULE + '.' + ext,
+#                     sources=[CYTHON_SOURCE_DIR + '/' + ext + '.pyx'],
+#                     include_dirs=INCLUDE_DIRS,
+#                     extra_compile_args=COMPILER_FLAGS,
+#                     extra_link_args=LINK_FLAGS,
+#                     language='c++')
+#     EXT_MODULES.append(mod)
+
 # Read long description from README.
 README_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)),
                            'README.md')
@@ -39,7 +76,7 @@ with open(README_PATH) as readme_file:
     README = readme_file.read()
 
 setup(
-    name="qiskit-openqasm2",
+    name="qiskit-terra",
     version="0.16.0",
     description="Software for developing quantum computing programs",
     long_description=README,
@@ -65,6 +102,7 @@ setup(
     keywords="qiskit sdk quantum",
     packages=find_packages(exclude=['test*']),
     install_requires=REQUIREMENTS,
+    setup_requires=['Cython>=0.27.1'],
     include_package_data=True,
     python_requires=">=3.6",
     extras_require={
@@ -79,5 +117,6 @@ setup(
         "Documentation": "https://qiskit.org/documentation/",
         "Source Code": "https://github.com/Qiskit/qiskit-terra",
     },
+    ext_modules=cythonize(EXT_MODULES),
     zip_safe=False
 )
